5fbdff5dfd937324c294d5845ae365d658fedc75
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-01-14 03:19:04 +0800

    Delete landslide_model directory
    Signed-off-by: Gin <142237326+ZeroIsStar@users.noreply.github.com>

--- landslide_model/__init__.py
+++
@@ -1,9 +0,0 @@
-from landslide_model.SCDunetpp import SCDUNetPP #2024
-from landslide_model.bisedenet import BiSeNet_DenseNet6 #2024
-from landslide_model.Unet3P import UNet3Plus_DeepSup
-from landslide_model.MFFEnet import DeepLab  # 2022
-from landslide_model.RIPF_Unet import RiPF_UNet  # 2023
-from landslide_model.transUnet2 import TransUNet2  # 2025
-from landslide_model.attention_deeplabv3plus import AttentionDeeplabV3plus  # 2024
-from landslide_model.RFA_ResUnet import BFA_resunet # 2024
-
